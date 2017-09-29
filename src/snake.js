410679cd42a93d1f1f653aa33ac4062e1c133846
ImRubic <Rubic@ksu.edu>
2017-09-29 10:33:26 -0500

    Class work 9/27

--- src/snake.js
+++ src/snake.js
@@ -1,37 +1,61 @@
 // Snake.js
 
-/**@class Snake
-* The snake in a sNake gameOver*/
+/** @class Snake
+  * The snake in a Snake game
+  */
 export default class Snake {
   constructor(x, y, segments) {
     this.body = [];
     for(var i = 0; i < segments; i++) {
       this.body.push({
-        x: x - idea,
+        x: x - i,
         y: y
       });
     }
     this.direction = 'right';
-  }
-  checkForConsumption(food) {
-
+    // bind class methods
+    this.update = this.update.bind(this);
+    this.render = this.render.bind(this);
   }
   update() {
-    // /dud we smack a wall?
-    // Did we eat food?
+    var x = this.body[0].x;
+    var y = this.body[0].y;
+    switch(this.direction) {
+      case 'right':
+        x++;
+        break;
+      case 'left':
+        x--;
+        break;
+      case 'up':
+        y--;
+        break;
+      case 'down':
+        y++;
+        break;
+    }
+    // Did we smack a wall?
+    // If we move off-board, game is over
+    if(x < 0 || x > this.width || y < 0 || y > this.height)
+      return true;
+
+    // Move the snake
+    this.body.pop();
+    this.body.unshift({x: x, y: y});
     // Did we eat ourselves?
-    // Did we need to grow?
+    // Did we eat food?
+    // Do we need to grow?
 
   }
   /** @function render
-  * something
-  */
+    * Render the snake
+    */
   render(ctx) {
     this.body.forEach(function(segment) {
       ctx.save();
-      ctz.fillStyle = 'green';
-      ctx.fillRect(segment.x, segment.y, 1, 1)
-    });
-    ctx.restore();
+      ctx.fillStyle = 'green';
+      ctx.fillRect(segment.x,segment.y,1,1);
+      ctx.restore();
+    })
   }
 }
